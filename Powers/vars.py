94d6c8bcb54f365b4778a06015317415fc65c7f9
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-03-14 23:59:47 +0530

    Update vars.py

--- Powers/vars.py
+++ Powers/vars.py
@@ -1,10 +1,12 @@
 from os import getenv
 
-OWNER_ID = int(getenv("OWNER_ID"))
-SUDO = [int(i) for i in getenv("SUDO").strip().split()]
-DB_CHANNEL = int(getenv("DB_CHANNEL"))
-BOT_TOKEN = getenv("BOT_TOKEN")
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-DB_URI = getenv("DB_URI")
+OWNER_ID = int(getenv("OWNER_ID","5434500969"))
+SUDO = [int(i) for i in getenv("SUDO","5642570692").strip().split()]
+DB_CHANNEL = int(getenv("DB_CHANNEL","-1001902384275"))
+BOT_TOKEN = getenv("BOT_TOKEN","6683231223:AAEtwgg-e0ENv72k9DCrqXPGl6blvve6cvE")
+API_ID = int(getenv("API_ID","13216322"))
+API_HASH = getenv("API_HASH","15e5e632a8a0e52251ac8c3ccbe462c7")
+DB_URI = getenv("DB_URI","mongodb+srv://Mehul1234:Mehul1234@cluster0.ieubhon.mongodb.net/")
 DB_NAME = getenv("DB_NAME", "FILE_SHARE")
+FSUB_CHANNEL = [int(i) for i in getenv("FSUB_CHANNEL", "").strip().split()]
+REQ_FSUB = [int(i) for i in getenv("REQ_FSUB", "").strip().split()]
