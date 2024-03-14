32222ea15b37d2d615e22600627d2a5b271d433d
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-03-15 00:31:49 +0530

    Update vars.py

--- Powers/vars.py
+++ Powers/vars.py
@@ -8,5 +8,5 @@ API_ID = int(getenv("API_ID","13216322"))
 API_HASH = getenv("API_HASH","15e5e632a8a0e52251ac8c3ccbe462c7")
 DB_URI = getenv("DB_URI","mongodb+srv://Mehul1234:Mehul1234@cluster0.ieubhon.mongodb.net/")
 DB_NAME = getenv("DB_NAME", "FILE_SHARE")
-FSUB_CHANNEL = [int(i) for i in getenv("FSUB_CHANNEL", "").strip().split()]
+FSUB_CHANNEL = [int(i) for i in getenv("FSUB_CHANNEL", "-1001755279044").strip().split()]
 REQ_FSUB = [int(i) for i in getenv("REQ_FSUB", "").strip().split()]
